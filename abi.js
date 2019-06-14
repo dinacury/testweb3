02ba9b0c712ed7a9042b4254ad8fb32a0f538a17
dinacury <50384637+dinacury@users.noreply.github.com>
2019-06-13 21:03:54 -0300

    outroabi.js

--- abi.js
+++ abi.js
@@ -1,106 +1,71 @@
 /* This is for example purposes */
-const contractABI = [
-	{
-		"constant": false,
-		"inputs": [
-			{
-				"name": "paramEndereco",
-				"type": "address"
-			},
-			{
-				"name": "paramNomeProprietario",
-				"type": "string"
-			},
-			{
-				"name": "paramValorVenal",
-				"type": "uint256"
-			}
-		],
-		"name": "registraImovel",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [
-			{
-				"name": "matricula",
-				"type": "uint256"
-			}
-		],
-		"name": "devolveNomeProprietario",
-		"outputs": [
-			{
-				"name": "",
-				"type": "string"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [
-			{
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"name": "livro1",
-		"outputs": [
-			{
-				"name": "endereco",
-				"type": "address"
-			},
-			{
-				"name": "nomeProprietario",
-				"type": "string"
-			},
-			{
-				"name": "valorVenal",
-				"type": "uint256"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [
-			{
-				"name": "",
-				"type": "address"
-			}
-		],
-		"name": "livro2",
-		"outputs": [
-			{
-				"name": "endereco",
-				"type": "address"
-			},
-			{
-				"name": "nomeProprietario",
-				"type": "string"
-			},
-			{
-				"name": "valorVenal",
-				"type": "uint256"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	}
-]
+const contractABI = pragma solidity 0.5.8;
 
+contract TimeCharterParty {
+    
+    string public Owners;
+    string public Charterers;
+    uint256 public Hire;
+    uint public FineFactor;
+    uint public DaysUnpaid;
+    uint public period;
+    uint public FineFactorLimit;
+    
+     struct Vessel {
+        string VesselName;
+        string Flag;
+        uint YearBuilt;
+        uint DeadWeight;
+        uint GrainCapacity;
+        uint SpeedCapability;
+    }
+    Vessel[] public Vessels;
+    string public PlaceofCharter;
+    string public DeliveryPort;
+    uint public EarliestDeliveryTime;
+    uint public CancellationTime;
+    uint public FuelQuantityOnDelivery;
+    uint public FuelQuantityOnRedelivery;
+    uint public FuelPriceOnDelivery;
+    uint public FuelPriceOnRedelivery;
+    string public OwnersBankAccount;
+    uint public MaxPeriodForRequisition;
+    uint public LumpsumForHoldCleaningOnRedelivery;
+    string public LawAndArbitration;
+    string public Comissioner;
+    uint public ComissionRate;
+    
+    constructor(
+        string memory _Owners,
+        string memory _Charterers,
+        uint HirePerDay,
+        uint FineFactorPerDay,
+        uint Days ) public {
+            
+        Owners = _Owners;
+        Charterers = _Charterers;
+        Hire = HirePerDay;
+        FineFactor = HirePerDay*2;
+        period = Days;
+        FineFactorLimit = HirePerDay*45;
+        
+        if (FineFactor > FineFactorLimit) {
+            require(FineFactorLimit >= FineFactorPerDay*DaysUnpaid);
+        }
+    
+    }
+    
+    function calculodaMulta() public view returns (uint256) {
+        return FineFactor*DaysUnpaid;
+    }
+    
+   
+    
+}
 if (network === "4") {
-    contractAddress = "0xbb99b6df9af619da29fa5c94420ebaa904046a79"; 
+    contractAddress = "0x9ae8dcceb5677bbdcf588c6b9f4f1ba0b9d67938"; 
 } else {
-    contractAddress = "0xbb99b6df9af619da29fa5c94420ebaa904046a79"; 
+    contractAddress = "0x9ae8dcceb5677bbdcf588c6b9f4f1ba0b9d67938"; 
 }
 
 contract = web3.eth.contract(contractABI).at(contractAddress);
